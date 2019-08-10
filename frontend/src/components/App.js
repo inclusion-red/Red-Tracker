6db0f4c46d964fa0afd03d5ce4bd2b0698a7c42e
Kwasi Amanfi <kamanfi@gmail.com>
2019-08-09 20:51:09 -0400

    setup splash/mainPage Route

---
+++ frontend/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route, HashRouter } from 'react-router-dom';
+
+import './App.css';
+
+
+import SplashPage from './splashPage/SplashPage';
+import MainPage from './mainPage/mainPage'
+
+
+
+const App = () => (
+
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={SplashPage} />
+      <Route exact path="/mainPage" component={MainPage} />
+    </Switch>
+  </HashRouter>
+
+);
+
+export default App;
+
---
+++ frontend/src/components/mainPage/mainPage.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import { Switch } from 'react-router-dom';
+
+function mainPage() {
+
+    return(
+
+        <div>
+            WELCOME DUDE!!
+        </div>
+    )
+}
+
+export default mainPage
