2e2cf23a2c8d2664c5981bc6648914e72bab9115
josue rojas <josuerojas.rojas@gmail.com>
2019-08-12 18:09:37 -0400

    fix lint error on jsx

--- frontend/src/components/form/FormFields/InputField.jsx
+++ frontend/src/components/form/FormFields/InputField.jsx
@@ -2,29 +2,29 @@ import React,{useState, useEffect}from 'react';
 import useForm from '../../customHooks/useForm';
 import QuestionForm from './QuestionForm';
 
-function InputField() {   
+function InputField() {
 
-      const [arows, UseArows] = useState(0);
+  const [arows, UseArows] = useState(0);
 
-      useEffect(() => {
-        const answerArea = document.getElementById("answer");
-        const ansRowCount = answerArea ? answerArea.value.split("\n").length : 0;
-        UseArows( ansRowCount + .5); 
-      });
+  useEffect(() => {
+    const answerArea = document.getElementById("answer");
+    const ansRowCount = answerArea ? answerArea.value.split("\n").length : 0;
+    UseArows( ansRowCount + .5);
+  });
 
-      const { inputs, handleInputChange, handleSubmit } = useForm();
+  const { inputs, handleInputChange } = useForm();
 
 
-    return (
+  return (
         <>
         <QuestionForm />
         <div className="field">
-            <div className="control">
-                <textarea className="textarea" id='answer' rows={arows} placeholder="Answer" name="answer" value={inputs.answer} onChange={handleInputChange}></textarea>
-            </div>
+          <div className="control">
+            <textarea className="textarea" id='answer' rows={arows} placeholder="Answer" name="answer" value={inputs.answer} onChange={handleInputChange}></textarea>
+          </div>
         </div>
         </>
-    )
+  )
 }
 
 export default InputField
