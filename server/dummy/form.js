33d7d16d39822e1eb4358d229f7ada63211dfaf1
josue rojas <josuerojas.rojas@gmail.com>
2019-08-12 13:55:51 -0400

    added basic dummy data

---
+++ server/dummy/form.js
@@ -0,0 +1,19 @@
+const Form = [
+  {
+    formid: 1,
+    createat: new Date(),
+    active: true
+  },
+  {
+    formid: 2,
+    createat: new Date(),
+    active: false
+  },
+  {
+    formid: 3,
+    createat: new Date(),
+    active: false
+  }
+];
+
+module.exports = Form;
---
+++ server/dummy/form_field.js
@@ -0,0 +1,37 @@
+const Form_Field = [
+  // form 1
+  {
+    formfieldid: 1,
+    formid: 1,
+    tag: 'input',
+    othersettings: {}
+  },
+  {
+    formfieldid: 2,
+    formid: 1,
+    tag: 'input',
+    othersettings: {}
+  },
+  {
+    formfieldid: 3,
+    formid: 1,
+    tag: 'input',
+    othersettings: {}
+  },
+  // form 2
+  {
+    formfieldid: 4,
+    formid: 2,
+    tag: 'input',
+    othersettings: {}
+  },
+  // form 3
+  {
+    formfieldid: 5,
+    formid: 3,
+    tag: 'input',
+    othersettings: {}
+  },
+];
+
+module.exports = Form_Field;
