6743d44bbec7f21151f118e6b632df5817311664
josue rojas <josuerojas.rojas@gmail.com>
2019-08-12 17:51:50 -0400

    fix linting errors

--- frontend/src/components/applicant/dummyDB/applicants.js
+++ frontend/src/components/applicant/dummyDB/applicants.js
@@ -4,10 +4,10 @@ import faker from 'faker';
 let applicants={};
 
 for(let i=0; i <= 10; i++){
-    applicants[i] ={
-        name: faker.name.findName(),
-        email: faker.internet.email(),
-    };
+  applicants[i] ={
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+  };
 }
 
 export default applicants;
--- frontend/src/components/customHooks/useForm.js
+++ frontend/src/components/customHooks/useForm.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 
 
 const useForm = (callback) => {
-    const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({});
 
 
-    const handleSubmit = (event) => {
-        if (event) event.preventDefault();
-        callback();
-    };
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+    callback();
+  };
 
-    const handleInputChange = (event) => {
-        event.persist();
-        setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
-    }
-    return {
-        handleSubmit,
-        handleInputChange,
-        inputs
-      };
+  const handleInputChange = (event) => {
+    event.persist();
+    setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
+  }
+  return {
+    handleSubmit,
+    handleInputChange,
+    inputs
+  };
 }
 export default useForm;
