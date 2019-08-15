ee5f0c8d128728c65bcaecae3edb9014480f945c
josue rojas <josuerojas.rojas@gmail.com>
2019-08-15 15:53:21 -0400

    fix responsive design

--- frontend/src/components/splashPage/SplashPage.js
+++ frontend/src/components/splashPage/SplashPage.js
@@ -13,56 +13,12 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 const useStyles = makeStyles(theme => ({
   card: {
-    width: "40%",
-    height: "40%",
-    "@media(max-width: 1038px)": {
-      width: "43%",
-      height: "42%"
+    width: "90%",
+    textAlign: "center",
+    "@media(min-width: 768px)": {
+      width: "40%",
     },
-    "@media(max-width: 970px)": {
-      width: "43%",
-      height: "45%"
-    },
-    "@media(max-width: 830px)": {
-      width: "45%",
-      height: "47%"
-    },
-    "@media(max-width: 790px)": {
-      width: "47%",
-      height: "50%"
-    },
-    "@media(max-width: 694px)": {
-      width: "50%",
-      height: "52%"
-    },
-    "@media(max-width: 655px)": {
-      width: "52%",
-      height: "55%"
-    },
-    "@media(max-width: 608px)": {
-      width: "55%",
-      height: "58%"
-    },
-    "@media(max-width: 507px)": {
-      width: "58%",
-      height: "61%"
-    },
-    "@media(max-width: 433px)": {
-      width: "60%",
-      height: "64%"
-    },
-    "@media(max-width: 410px)": {
-      width: "62%",
-      height: "67%"
-    },
-    "@media(max-width: 381px)": {
-      width: "65%",
-      height: "70%"
-    },
-    margin: "10% auto",
-    position: "absolute",
-    top: "15%",
-    left: "30%",
+    margin: "auto",
     backgroundColor: "rgba(255, 255, 255, 0.8)",
     overflow: "visible"
   },
