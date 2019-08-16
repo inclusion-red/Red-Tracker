a76040ab18b6ea4a9ebc07c347e2b7eb65e9a7e3
Kwasi Amanfi <kamanfi@gmail.com>
2019-08-16 17:54:39 -0400

    add forms

--- frontend/src/components/form/FormFields/QuestionForm.jsx
+++ frontend/src/components/form/FormFields/QuestionForm.jsx
@@ -13,20 +13,27 @@ function QuestionForm(props) {
     const questionRowCount = questionArea ? questionArea.value.split(/\r?\n/).length : 0;
     UseQrows(questionRowCount + .5);
   });
-  
+
   useEffect(() =>{
     if(props.answerTag === 'input' && props.save === true){
-      let answerTag = {
-          formid: 3,
-          tag: 'textarea',
-          classes: 'textarea',
-          question: inputs.question
-      };
-      test(answerTag);
+      let userData={
+        "active": true,
+        "title": "tempForm",
+        "formfields": [
+            {
+                "tag": "input",
+                "question": inputs.question,
+                "class": "",
+                "other_setting": null
+            },
+        ]
+    }
+      test(userData);
     }
   })
-
+ 
   return (
+
     <div className="field">
       <div className="control">
         <textarea className="textarea has-background-primary" id='question' rows={qrows} placeholder="Question" name="question" value={inputs.question} onChange={handleInputChange}></textarea>
