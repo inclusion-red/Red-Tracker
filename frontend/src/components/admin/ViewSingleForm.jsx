4d79e70a094772d7dd4e13f216bd8fc8e4fa655c
Ziwei Song <ydnic622@gmail.com>
2019-08-18 22:34:18 -0400

    Frontend: Admin view signler form submission from applicant

--- frontend/src/components/admin/ViewSingleForm.jsx
+++ frontend/src/components/admin/ViewSingleForm.jsx
@@ -4,30 +4,49 @@ import PropTypes from 'prop-types';
 // this page is to show a single form for an admin to update or change
 
 export default class ViewSingleFormAdmin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      form: {},
+    constructor(props) {
+      super(props);
+      this.state = {
+        form: {
+          title: '',
+          formfields: [],
+        }
+      }
+    }
+  
+    // should return only if it is active if not then display an error (probably should be a different route)
+    componentWillMount() {
+      fetch(`/api/form/${this.props.match.params.formid}`)
+        .then( d => d.json() )
+        .then( form => this.setState({form}) )
+        .catch( e => console.log('something went wrong', e))
+    }
+
+    
+    render() {
+      console.log(this.state);
+      const {title, formfields} = this.state.form;
+      return(
+        <form>
+          <p>{title}</p>
+          <label>
+            Email:
+            <input type="text" value={this.state.applicant.email}/>
+          </label>
+          {formfields.map(field => {
+            return(
+              <label key={field.id}>
+                {field.question}
+                <field.tag type="text" value={this.state.response[field.id]} />
+              </label>
+            )
+          })}
+          <button>Submit</button>
+        </form>
+      )
     }
   }
-
-  componentWillMount() {
-    fetch(`/api/form/${this.props.match.params.formid}`)
-      .then( d => d.json() )
-      .then( form => this.setState({form}) )
-      .catch( e => console.log('something went wrong', e))
-  }
-
-  render() {
-    // console.log('forms stuff', this.state.form);
-    return(
-      <div>
-        {this.state.form.title}
-        Admin form { this.props.match.params.formid }
-      </div>
-    )
-  }
-}
+  
 
 ViewSingleFormAdmin.propTypes = {
   match: PropTypes.object
